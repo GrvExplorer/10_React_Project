b937da955c7fa68abdc44f826cf1bafbaa23a546
GrvExplorer <icodelife307@gmail.com>
2024-05-20 17:46:21 +0530

    support

--- 10 Projects/Project-6-Crypto/src/pages/Support/Support.jsx
+++ 10 Projects/Project-6-Crypto/src/pages/Support/Support.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import { CiMail } from "react-icons/ci";
 
 function Support() {
   return (
-    <div>Support</div>
-  )
+    <div className="flex flex-col gap-24">
+      <div className="mx-auto flex w-full max-w-[1024px] justify-between gap-4  md:px-10">
+        <div className="w-[450px]">
+          <div>
+            <img src="./Email.png" alt="email" className="h-6 w-6" />
+          </div>
+          <p className="mt-2 text-2xl font-semibold">Contract Us</p>
+          <p className="text-sm text-Gray">
+            Have a question or just want to know more? Feel free to reach out to
+            us.
+          </p>
+        </div>
+        <form
+          action=""
+          className="flex w-[550px] flex-col gap-6 rounded-2xl bg-White p-4"
+        >
+          <p className="text-sm font-semibold">
+            You will receive response within 24 hours of time of submit.
+          </p>
+          <div className="flex justify-between">
+            <label htmlFor="name" className="font-semibold">
+              Name
+              <input
+                type="text"
+                placeholder="James"
+                className="rounded-md border px-3 py-1 font-normal outline-none"
+                id="name"
+              />
+            </label>
+            <label htmlFor="surname" className="font-semibold">
+              Surname
+              <input
+                type="text"
+                placeholder="Arthur"
+                className="w-full rounded-md border px-3 py-1 font-normal outline-none"
+                id="surname"
+              />
+            </label>
+          </div>
+          <label htmlFor="email" className="flex flex-col font-semibold">
+            Email
+            <input
+              type="email"
+              placeholder="name@mail.com"
+              className="rounded-md border px-3 py-1 font-normal outline-none"
+              id="email"
+            />
+          </label>
+
+          <label htmlFor="message" className="flex flex-col font-semibold">
+            Message
+            <textarea
+              className="h-24 rounded-md border px-3 py-1 font-normal outline-none"
+              name="message"
+              id="message"
+              placeholder="Your Message"
+            ></textarea>
+          </label>
+          <label htmlFor="t&c" className="flex gap-2">
+            <input type="checkbox" name="t&c" id="t&c" />I agree with{" "}
+            <span className="text-Purple">Terms & Conditions.</span>
+          </label>
+
+          <div className="flex flex-col gap-2">
+            <button className="rounded-lg bg-[#D8DDE2] py-2 font-semibold text-[#797E82] ">
+              Send a Message
+            </button>
+            <button className="rounded-lg bg-Main py-2 font-semibold">
+              Send a Message
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mx-auto flex w-full max-w-[1024px] justify-between gap-4  md:px-10">
+        <div
+          className="w-[450px]
+  
+    "
+        >
+          <div>
+            <img src="./Chatbot.png" alt="email" className="h-6 w-6" />
+          </div>
+          <p className="mt-2 text-2xl font-semibold">Live Chat</p>
+          <p className="text-sm text-Gray">
+            Don’t have time to wait for the answer? Chat with us now.
+          </p>
+        </div>
+
+        <div className="w-[550px] rounded-lg bg-Purple p-6">
+          <p className="w-fit rounded-2xl bg-White px-2 py-1 text-sm font-semibold text-Purple mb-2">
+            Chatbot{" "}
+          </p>
+
+          <p className="text-White">
+       Chat with us now
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Support
+export default Support;
--- 10 Projects/Project-6-Crypto/src/pages/Transaction/Transaction.jsx
+++ 10 Projects/Project-6-Crypto/src/pages/Transaction/Transaction.jsx
@@ -1,19 +1,72 @@
+import { FaAngleDown } from "react-icons/fa";
 import { IoMdDownload } from "react-icons/io";
 import { IoSearchOutline } from "react-icons/io5";
 
 function Transaction() {
+  const transactionsLoad = [
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "pending",
+    },
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "processing",
+    },
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "cancelled",
+    },
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "completed",
+    },
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "completed",
+    },
+    {
+      id: "HD82NA2H",
+      data: "2022-06-09",
+      time: "7:06 PM",
+      type: "INR Deposit",
+      amount: "+ ₹81,123.10",
+      status: "completed",
+    },
+  ];
+
   return (
-    <div className="mx-auto min-w-[1024px] max-w-[1024px]">
-      <div className="text-end">
+    <div className="mx-auto min-w-[1024px] max-w-[1024px] md:px-10">
+      <div className="mb-4 flex justify-end text-end">
         <button className="text-md flex items-center justify-center gap-2 rounded-lg bg-Purple px-3 py-3 font-bold text-White ">
           <IoMdDownload className="text-xl" />
           Export CSV
         </button>
       </div>
 
-      <div className="h-fit w-full rounded-lg bg-White px-6 py-4">
-        <div className="flex justify-between">
-          <div className="flex gap-4 ">
+      <div className="h-fit w-full rounded-2xl bg-White px-6 py-4">
+        {/* Navigation Of transactions */}
+        <div className="flex items-center justify-between border-b-2 pl-4">
+          <div className="flex gap-10">
             <div className="flex items-center gap-3 border-b-4 border-transparent pb-2 font-semibold text-black hover:border-Purple">
               All
               <p className="rounded-xl bg-Main p-1 text-Gray">349</p>
@@ -31,9 +84,7 @@ function Transaction() {
               <p className="rounded-xl bg-Main p-1 text-Gray">22</p>
             </div>
           </div>
-          <div
-            className="flex items-center gap-1 border-b-4 border-none"
-          >
+          <div className="flex gap-1 border-b-4 border-none">
             <IoSearchOutline className="text-2xl text-Gray" />
             <input
               type="text"
@@ -42,6 +93,46 @@ function Transaction() {
             />
           </div>
         </div>
+
+        <div className="flex w-full gap-32 border-b-2 px-2 py-4 font-semibold justify-center text-Gray ">
+          <p className="">ID</p>
+          <p className="flex items-center gap-1">
+            Data & Time <FaAngleDown />
+          </p>
+          <p className="">Type</p>
+          <p className="flex items-center gap-1">
+            Amount <FaAngleDown />
+          </p>
+          <p className="">Status</p>
+        </div>
+
+        {transactionsLoad.map(({ id, data, time, type, amount, status }, i) => (
+          <div
+            key={i}
+            className="mt-6 flex w-full items-center gap-24 border-b px-2 py-4 font-bold border-opacity-0"
+          >
+            <p className="">{id}</p>
+            <p className="flex  flex-col justify-center">
+              {data}
+              <p className="font-normal text-Gray">{time}</p>
+            </p>
+            <p className="">{type}</p>
+            <p className="flex items-center gap-1">{amount}</p>
+            <p
+              className={`
+          ${status == "pending" && "bg-Gray"}
+          ${status == "processing" && "bg-yellow-500"}
+          ${status == "cancelled" && "bg-red-500"}
+          ${status == "completed" && "bg-green-500"}
+h-fit rounded-2xl px-2 py-1 text-white`}
+            >
+              {status == "pending" && "Pending"}
+              {status == "processing" && "Processing"}
+              {status == "cancelled" && "Cancelled"}
+              {status == "completed" && "Completed"}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
